bc698367c68814295c27d7bb96c75d84484f4ae3
iarp <ian@iarp.ca>
2019-07-23 16:27:06 -0400

    Update validate_pdf to also check value type is what is expected.

--- django_pdf_filler/validators.py
+++ django_pdf_filler/validators.py
@@ -1,8 +1,17 @@
 from PyPDF2 import PdfFileReader
 from django.core.exceptions import ValidationError
+from django.db.models.fields.files import FieldFile
 
 
 def validate_pdf(value):
+    """ Ensure the value supplied is a proper PDF by checking its given extension
+        AND whether or not it can be read by PyPDF2
+
+    :param value: FieldFile object of the uploaded file.
+    """
+
+    if not isinstance(value, FieldFile):
+        raise ValidationError('Object supplied is not a File.')
 
     if not value.name.endswith('.pdf'):
         raise ValidationError('File supplied is not a Document')
