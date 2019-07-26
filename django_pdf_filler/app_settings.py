467a327c4eb1a236c31d73a15dc7746c002bc26c
iarp <ian@iarp.ca>
2019-07-26 11:12:36 -0400

    Updated app_settings to use a class based system

--- django_pdf_filler/app_settings.py
+++ django_pdf_filler/app_settings.py
@@ -1,33 +1,75 @@
-import os
+class AppSettings(object):
 
-from django.conf import settings
+    def __init__(self, prefix):
+        self.prefix = prefix
 
-GENERATE_LAYOUT_IMAGE = getattr(settings, 'DJANGO_PDF_FILLER_GENERATE_LAYOUT_IMAGE', True)
+        assert isinstance(self.FIELD_VALUE_JOINS, (str, list, set, tuple)), \
+            "DJANGO_PDF_FILLER_FIELD_VALUE_JOINS must be of type list, set, tuple, or str"
+        assert self.FIELD_CHAIN_SPLITTER != self.FIELD_DATETIME_SPLITTER, \
+            "DJANGO_PDF_FILLER_FIELD_CHAIN_SPLITTER and " \
+            "DJANGO_PDF_FILLER_FIELD_DATETIME_SPLITTER cannot be the same value."
+        assert isinstance(self.MAGICK_LOCATION, list), \
+            "DJANGO_PDF_FILLER_MAGICK_LOCATION must be of type list or str"
 
-LOCAL_DOCUMENT_STORAGE = getattr(
-    settings, 'DJANGO_PDF_FILLER_LOCAL_DOCUMENT_STORAGE',
-    os.path.join(settings.BASE_DIR, 'media', 'django_pdf_filler', 'documents')
-)
+    def _setting(self, name, default):
+        from django.conf import settings
+        return getattr(settings, self.prefix + name, default)
 
-MAGICK_DENSITY = str(getattr(settings, 'DJANGO_PDF_FILLER_MAGICK_DENSITY', 300))
-MAGICK_FLATTEN = getattr(settings, 'DJANGO_PDF_FILLER_MAGICK_FLATTEN', True)
-MAGICK_LOCATION = getattr(settings, 'DJANGO_PDF_FILLER_MAGICK_LOCATION', None)
+    @property
+    def GENERATE_LAYOUT_IMAGE(self):
+        return self._setting('GENERATE_LAYOUT_IMAGE', True)
 
-if MAGICK_LOCATION:
-    if isinstance(MAGICK_LOCATION, str):
-        MAGICK_LOCATION = [MAGICK_LOCATION]
-else:
-    MAGICK_LOCATION = ['/usr/bin/convert']
-    if os.name == 'nt':
-        MAGICK_LOCATION = ['magick.exe', 'convert']
+    @property
+    def LOCAL_DOCUMENT_STORAGE(self):
+        from django.conf import settings
+        import os
+        return self._setting(
+            'LOCAL_DOCUMENT_STORAGE',
+            os.path.join(settings.BASE_DIR, 'media', 'django_pdf_filler', 'documents')
+        )
 
-FIELD_VALUE_JOINS = getattr(settings, 'DJANGO_PDF_FILLER_FIELD_VALUE_JOINS', ', .|-_')
-FIELD_CHAIN_SPLITTER = getattr(settings, 'DJANGO_PDF_FILLER_FIELD_CHAIN_SPLITTER', '|')
-FIELD_DATETIME_SPLITTER = getattr(settings, 'DJANGO_PDF_FILLER_FIELD_DATETIME_SPLITTER', ':')
+    @property
+    def MAGICK_DENSITY(self):
+        return str(self._setting('MAGICK_DENSITY', 300))
 
-assert isinstance(FIELD_VALUE_JOINS, (str, list, set, tuple)), \
-    "DJANGO_PDF_FILLER_FIELD_VALUE_JOINS must be of type list, set, tuple, or str"
-assert FIELD_CHAIN_SPLITTER != FIELD_DATETIME_SPLITTER, \
-    "DJANGO_PDF_FILLER_FIELD_CHAIN_SPLITTER and DJANGO_PDF_FILLER_FIELD_DATETIME_SPLITTER cannot be the same value."
-assert isinstance(MAGICK_LOCATION, list), \
-    "DJANGO_PDF_FILLER_MAGICK_LOCATION must be of type list or str"
+    @property
+    def MAGICK_FLATTEN(self):
+        return self._setting('MAGICK_FLATTEN', True)
+
+    @property
+    def MAGICK_LOCATION(self):
+        import os
+        location = self._setting('MAGICK_LOCATION', None)
+
+        if location:
+            if isinstance(location, str):
+                location = [location]
+        else:
+            location = ['/usr/bin/convert']
+            if os.name == 'nt':
+                location = ['magick.exe', 'convert']
+
+        return location
+
+    @property
+    def FIELD_VALUE_JOINS(self):
+        return self._setting('FIELD_VALUE_JOINS', ', .|-_')
+
+    @property
+    def FIELD_CHAIN_SPLITTER(self):
+        return self._setting('FIELD_CHAIN_SPLITTER', '|')
+
+    @property
+    def FIELD_DATETIME_SPLITTER(self):
+        return self._setting('FIELD_DATETIME_SPLITTER', ':')
+
+    @property
+    def COMMANDS(self):
+        return self._setting('COMMANDS', 'django_pdf_filler.commands.DefaultCommands')
+
+
+import sys  # noqa
+
+app_settings = AppSettings('DJANGO_PDF_FILLER_')
+app_settings.__name__ = __name__
+sys.modules[__name__] = app_settings
