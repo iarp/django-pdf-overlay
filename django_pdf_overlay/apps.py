20df85c5696bdb254636e9d875f1a3d21f09b964
iarp <iarp.opensource@gmail.com>
2025-03-24 16:53:43 -0400

    removal of six as a requirement

--- django_pdf_overlay/apps.py
+++ django_pdf_overlay/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 
 class DjangoPDFOverlayConfig(AppConfig):
     name = 'django_pdf_overlay'
+    default_auto_field = "django.db.models.AutoField"
 
     def ready(self):
         from . import signals  # noqa, flake8 issue
